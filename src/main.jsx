1ba32af9b5abb9a99c8416269a4344c49d32ef17
Collins-cl <collinsolads@gmail.com>
2023-12-18 15:29:46 +0100

    bonk5.0

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./Fonts/bear-hug/Bear-hug.ttf";
+import "./Fonts/bear-hug/Bear-Hug.ttf";
 import "./Fonts/Montserrat/static/Montserrat-Regular.ttf";
 import "./Fonts/Montserrat/static/Montserrat-Bold.ttf";
 
